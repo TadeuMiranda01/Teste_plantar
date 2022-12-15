9bf438c48b9942c9d77f1c3bfb05dfbf18957e53
TadeuMiranda01 <45497302+TadeuMiranda01@users.noreply.github.com>
2022-12-15 18:21:20 -0300

    Add files via upload

---
+++ frontend/src/views/Cidades.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="about">
+      <h1>This is an cidades page</h1>
+      <hr>
+        <div v-for="cidade in cidades">
+            {{cidade.grupo_id}}<br>
+            {{cidade.nome}}<br>
+            {{cidade.idade}}<br>
+            {{cidade.estado}}<br>
+        </div>
+    </div>
+  </template>
+
+<script>
+
+
+
+    export default {
+        data() {
+            return {
+                cidades: []
+            }
+        },
+
+        created(){
+            fetch('http://localhost:8000/api/cidade')
+            .then(response => response.json())
+            .then(r => {
+                this.cidades = r;
+            })
+
+        }
+    }
+
+</script>
+  
